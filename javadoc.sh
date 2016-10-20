e5ad09e40f830fa9922a9df7c0bb9ab1e4f2d4fe
Moxie Marlinspike <moxie@thoughtcrime.org>
2016-10-19 17:16:58 -0700

    updated javadoc

--- javadoc.sh
+++ javadoc.sh
@@ -1,4 +1,4 @@
 rm -rf ./tmp/
-git clone https://github.com/WhisperSystems/libtextsecure-java.git tmp
-javadoc -doclet com.google.doclava.Doclava -docletpath doclava-1.0.6.jar -d . -sourcepath tmp/java/src/main/java/ -subpackages org -hdf project.name "libtextsecure-java"
+git clone https://github.com/WhisperSystems/libsignal-service-java.git tmp
+javadoc -doclet com.google.doclava.Doclava -docletpath doclava-1.0.6.jar -d . -sourcepath tmp/java/src/main/java/ -subpackages org -hdf project.name "libsignal-service-java"
 rm -rf ./tmp/
